9dd59f875521a506d55de2f4d59e083309068296
shailendra-rgb <shailendra@voicemanagementconsultancy.com>
2026-09-10 17:47:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const https = require("https");
 
-console.log("=== ZEpto Network Test ===");
+const options = {
+  hostname: "api-gateway.zepto.co.in",
+  path: "/rvms/api/v1/otp/vendor/send-otp",
+  method: "OPTIONS",
+  headers: {
+    "Origin": "https://saathi.zepto.co.in",
+    "Access-Control-Request-Method": "POST",
+    "Access-Control-Request-Headers": "authorization,content-type"
+  }
+};
 
-https.get("https://api-gateway.zepto.co.in/", (res) => {
+const req = https.request(options, (res) => {
   console.log("STATUS:", res.statusCode);
   console.log("HEADERS:", res.headers);
 
   let body = "";
-
   res.on("data", chunk => body += chunk);
-
   res.on("end", () => {
     console.log("BODY:", body);
-    process.exit(0);
   });
-
-}).on("error", (err) => {
-  console.error("ERROR:", err.message);
-  process.exit(1);
 });
+
+req.on("error", err => console.error("ERROR:", err));
+req.end();
